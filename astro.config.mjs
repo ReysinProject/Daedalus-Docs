0ec175e13afce0b2e8b38ac250ba6fdff64b3bb9
ER28 <l.guerra-rago@pm.me>
2025-01-19 17:24:30 +0100

    rework the main page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,24 +2,31 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Daedalus',
-			social: {
-				github: 'https://github.com/ReysinProject/Daedalus',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					autogenerate: { directory: 'guides' },
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
+    integrations: [starlight({
+        title: 'Daedalus',
+        social: {
+            github: 'https://github.com/ReysinProject/Daedalus',
+        },
+        sidebar: [
+            {
+                label: 'Guides',
+                autogenerate: { directory: 'guides' },
+            },
+            {
+                label: 'Reference',
+                autogenerate: { directory: 'reference' },
+            },
+        ],
+        customCss: [
+            './src/tailwind.css',
+        ]
 		}),
-	],
+        tailwind({
+            applyBaseStyles: false,
+        })
+    ],
 });
