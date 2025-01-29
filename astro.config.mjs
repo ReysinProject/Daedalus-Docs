9738915a97b4f3772678a5e2b1c37ec62c8fc07c
ER28 <l.guerra-rago@pm.me>
2025-01-29 10:27:23 +0100

    google

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'http://reysin.fr',
     integrations: [starlight({
         title: 'Daedalus',
         social: {
